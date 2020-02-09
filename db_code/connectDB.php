2c0e1a8643b0304dd90334fbe8dadcc21bb1f8a3
joselilove <macayananjoselin@gmail.com>
2020-02-09 22:51:04 +0800

    setup remote db

--- db_code/connectDB.php
+++ db_code/connectDB.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect("localhost","root","","shop");
+$connect = mysqli_connect("remotemysql.com","RIbr1K8N3J","LgD1CyPNhU","RIbr1K8N3J");
 		if(!$connect){
 			die('could not connect');
 		}
